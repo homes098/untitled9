98c1c43e05a572703e1dd774b647c3bed0769970
itcast <hello@itcast.cn>
2022-08-25 19:02:18 +0800

    "这是united09的信息"

---
+++ untitled/src/main/java/Test01.java
@@ -0,0 +1,10 @@
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+
+public class Test01 {
+    public static void main(String[] args) {
+        String a="aaab";
+
+    assert (a=="aab");
+        System.out.println(a);
+    }
+}
